ee49ec782bf5f44938b6b6d21becf4cc045b69ab
SamCito11 <samborda@misena.edu.co>
2024-09-19 22:40:02 -0500

    ok

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Importar cors
+const cors = require('cors'); // Asegúrate de tener esta librería instalada
 require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({ // Habilitar CORS
-    origin: ['http://localhost:5173', 'http://localhost:5174'],// Permitir solicitudes desde tu aplicación React
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-    credentials: true // Permitir credenciales si es necesario
-}));
+
+// Configuración de CORS
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174'], // Agrega todos los orígenes que necesites
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+    credentials: true, // Permite enviar cookies o datos de autenticación
+};
+
+app.use(cors(corsOptions)); // Aplicar las opciones de CORS
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Conectado a MongoDB'))
